01a6d90a157708c6e2e6fcb35512d24fb4370dad
mustafa-mahmoud203 <must.afa000262@gmail.com>
2025-02-10 15:06:20 +0200

    [+] add  rateLimit & compression

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import compression from "compression";
 
 import AppError from './utils/appError.js';
 import ErrorHandling from './middlewares/globalErrorHandling.js';
@@ -11,22 +13,31 @@ class App {
         private errorHandling: ErrorHandling
     ) {
         this.app = express()
-        this.port = this.port = parseInt(process.env.PORT || '3000', 10);
+        this.port = parseInt(process.env.PORT || '3000', 10);
 
         this.setMiddleware();
         this.initRoutes()
         this.setErrorHandling()
     }
     setMiddleware() {
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(compression());
+
+        const limiter = rateLimit({
+            windowMs: 15 * 60 * 1000,
+            max: 100,
+            message: "Too many requests from this IP, please try again later.",
+        });
+        this.app.use(limiter);
+
         this.app.use(
             cors({
-                origin: "http://localhost:3000",
+                origin: process.env.CORS_ORIGIN,
                 methods: ["GET", "POST", "DELETE", "PATCH", "OPTIONS"],
                 allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
             })
         );
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
     }
 
     initRoutes() {
